b319fad37e161397cb739c6c80f8d119179eba06
Netsurf Administrators <help@netsurf-browser.org>
2012-07-01 06:22:13 +0100

    prep

---
+++ hooks/post-receive.lua
@@ -0,0 +1,15 @@
+-- Post-receive hook for NetSurf repo, let's inform CIA of what's happening
+local repo, updates = ...
+
+empty = "0000000000000000000000000000000000000000"
+
+for ref, data in pairs(updates) do
+   if ref:sub(1,11) == "refs/heads/" and data.oldsha ~= empty and data.newsha ~= empty then
+      log.chat("Informing CIA of commits on", ref:sub(12))
+      cia.inform_commits("libparserutils", ref:sub(12), repo,
+			 data.oldsha, data.newsha)
+   else
+      log.chat("Not informing CIA about", ref)
+   end
+end
+
